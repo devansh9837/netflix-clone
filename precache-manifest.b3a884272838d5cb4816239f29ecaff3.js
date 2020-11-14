39a4e89a86e9eac89ef97dc77c018bf3dc07c818
devansh9837 <devanshagarwal2020@gmail.com>
2020-11-14 21:40:08 +0530

    Updates

---
+++ precache-manifest.b3a884272838d5cb4816239f29ecaff3.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66009d3ae19325a89b130191e1652932",
+    "url": "/darwin619/Netflix-Clone/index.html"
+  },
+  {
+    "revision": "cfc0a9f2c55a13dbfdf2",
+    "url": "/darwin619/Netflix-Clone/static/css/1.c1c197bd.chunk.css"
+  },
+  {
+    "revision": "75322c66be88ef673a38",
+    "url": "/darwin619/Netflix-Clone/static/css/5.afea43fe.chunk.css"
+  },
+  {
+    "revision": "d12580a6ba5c9470e5d7",
+    "url": "/darwin619/Netflix-Clone/static/css/6.afea43fe.chunk.css"
+  },
+  {
+    "revision": "8c516e08a0828c8b66f9",
+    "url": "/darwin619/Netflix-Clone/static/css/7.4ec44fee.chunk.css"
+  },
+  {
+    "revision": "647e5f300ea6a60ca358",
+    "url": "/darwin619/Netflix-Clone/static/css/8.4ec44fee.chunk.css"
+  },
+  {
+    "revision": "b2cb22ffa93823ddbc68",
+    "url": "/darwin619/Netflix-Clone/static/css/main.50e19095.chunk.css"
+  },
+  {
+    "revision": "ce6dbf669ac772907c2f",
+    "url": "/darwin619/Netflix-Clone/static/js/0.47e39d0f.chunk.js"
+  },
+  {
+    "revision": "cfc0a9f2c55a13dbfdf2",
+    "url": "/darwin619/Netflix-Clone/static/js/1.e9cd3901.chunk.js"
+  },
+  {
+    "revision": "e0ef4bc854d8f9a751ff",
+    "url": "/darwin619/Netflix-Clone/static/js/4.08f3996d.chunk.js"
+  },
+  {
+    "revision": "75322c66be88ef673a38",
+    "url": "/darwin619/Netflix-Clone/static/js/5.bfef5348.chunk.js"
+  },
+  {
+    "revision": "d12580a6ba5c9470e5d7",
+    "url": "/darwin619/Netflix-Clone/static/js/6.81174077.chunk.js"
+  },
+  {
+    "revision": "8c516e08a0828c8b66f9",
+    "url": "/darwin619/Netflix-Clone/static/js/7.10a20e48.chunk.js"
+  },
+  {
+    "revision": "647e5f300ea6a60ca358",
+    "url": "/darwin619/Netflix-Clone/static/js/8.8f94aacc.chunk.js"
+  },
+  {
+    "revision": "b2cb22ffa93823ddbc68",
+    "url": "/darwin619/Netflix-Clone/static/js/main.7a72eaa1.chunk.js"
+  },
+  {
+    "revision": "2605a540bba41c353ed1",
+    "url": "/darwin619/Netflix-Clone/static/js/runtime~main.13a36d42.js"
+  },
+  {
+    "revision": "d6d290478902fbbe09c620b4aaba985f",
+    "url": "/darwin619/Netflix-Clone/static/media/AIMsqmnV.d6d29047.mp3"
+  },
+  {
+    "revision": "d2edb29f3f970c36aadecbb01ed0bb79",
+    "url": "/darwin619/Netflix-Clone/static/media/Background.d2edb29f.jpg"
+  },
+  {
+    "revision": "369787c5653cd811d0828096039cc933",
+    "url": "/darwin619/Netflix-Clone/static/media/cHBfBHfv.369787c5.mp3"
+  },
+  {
+    "revision": "3a36d79cdda4606c11565122b4edd691",
+    "url": "/darwin619/Netflix-Clone/static/media/cSWrcZcB.3a36d79c.png"
+  },
+  {
+    "revision": "f7f2904f1f3f39d9b1ff2400c30bcecc",
+    "url": "/darwin619/Netflix-Clone/static/media/imdb.f7f2904f.png"
+  },
+  {
+    "revision": "a1f673e7df715f16dae49f4874009082",
+    "url": "/darwin619/Netflix-Clone/static/media/logo.a1f673e7.png"
+  },
+  {
+    "revision": "d902c77253bec61f403a5978d2e7c858",
+    "url": "/darwin619/Netflix-Clone/static/media/netflix.d902c772.jpg"
+  },
+  {
+    "revision": "fd3ce8aa9f2030a21f7ba0724f58ce8c",
+    "url": "/darwin619/Netflix-Clone/static/media/rsRlAjMp.fd3ce8aa.png"
+  },
+  {
+    "revision": "9179862c82e20d655c43c9b468b96aa4",
+    "url": "/darwin619/Netflix-Clone/static/media/star.9179862c.png"
+  }
+]);
